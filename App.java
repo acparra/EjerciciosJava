cc65439c8e09158da0b75124095b6a4b95d2de52
Parra-Andres <acpdelgadillo@gmail.com>
2021-08-18 12:51:19 -0500

    Ejercicio realizado

--- App.java
+++ App.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class App {
+
+    private static double[] TurnosM = new double[2]; 
+    private static double[] TurnosN = new double[2];
+    private static Scanner scanner = new Scanner(System.in); 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ingresarEmpleadosM();
+        ingresarEmpleadosN();
+        System.out.println("El total de los sueldos de la mañana es: " + obtenerSueldos(TurnosM));
+        System.out.println("El total de los sueldos de la noche es: " + obtenerSueldos(TurnosN));
     }
+
+    private static void ingresarEmpleadosM(){
+        System.out.println("Ingrese los sueldos de los empleado del turno de la mañana");
+        for (int i = 0; i < TurnosM.length; i++){
+            System.out.println("Ingrese el sueldo del empleado " + i + ": ");
+            TurnosM[i] = scanner.nextDouble();
+        }
+
+    }
+    private static void ingresarEmpleadosN(){
+        System.out.println("Ingrese los sueldos de los empleado del turno de la noche");
+        for (int i = 0; i < TurnosN.length; i++){
+            System.out.println("Ingrese el sueldo del empleado " + i + ": ");
+            TurnosN[i] = scanner.nextDouble();
+        } 
+    }
+
+    private static double obtenerSueldos(double[] Vector){
+        double Sueldo = 0;
+        for (int i = 0; i < Vector.length; i++){
+            Sueldo += Vector[i];
+        }
+        return Sueldo;
+    }
+
+
 }
